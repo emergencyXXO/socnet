5b4bbdc88c091a09d70bfc848eceafbd2f628a16
jekap <jekapoberezhnui@gmail.com>
2019-07-09 18:09:27 +0300

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Aside from './components/Aside';
-import Content from './components/Content';
+import Header from './components/Header/';
+import Aside from './components/Aside/';
+import Content from './components/Content/';
 
 function App() {
   return (
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import cls from './style.module.css';
+function Header() {
+    return (
+
+        <header>
+            <div className="container">
+                <div className={cls.logo} style={{backgroundImage: 'url(https://www.freeiconspng.com/uploads/flame-logo-png-30.png)'}}></div>
+                <p className={cls.header}>SocNet</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;

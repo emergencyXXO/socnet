3906fc9178ebce7145fbab2f990f10e41406579d
jekap <eugene.pob@leverage.it>
2019-10-09 15:51:33 +0300

    Updates

---
+++ precache-manifest.bbcab38060adab1c795a3d96749d910d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2a563011c04ac425a4e1dcd3069c21",
+    "url": "/socnet/index.html"
+  },
+  {
+    "revision": "3e4f0db246af552d15ab",
+    "url": "/socnet/static/css/main.573dbdc3.chunk.css"
+  },
+  {
+    "revision": "1826b6d188547c8f6f04",
+    "url": "/socnet/static/js/2.6a109ab1.chunk.js"
+  },
+  {
+    "revision": "3e4f0db246af552d15ab",
+    "url": "/socnet/static/js/main.299c3b9b.chunk.js"
+  },
+  {
+    "revision": "8fc0a0c0c4a92393c1c4",
+    "url": "/socnet/static/js/runtime~main.5989f925.js"
+  }
+]);
